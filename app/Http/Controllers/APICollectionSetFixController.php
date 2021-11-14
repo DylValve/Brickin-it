dd4c9f8eca5abf97af681c90ad5ca66ba85bf2a5
DylValve <20047221@tafe.wa.edu.au>
2021-11-14 17:12:20 +0800

    Collection Set Delete

--- app/Http/Controllers/APICollectionSetFixController.php
+++ app/Http/Controllers/APICollectionSetFixController.php
@@ -59,7 +59,7 @@ class APICollectionSetFixController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(CollectionSetFix $cSFC)
     {
